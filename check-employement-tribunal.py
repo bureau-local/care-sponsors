279e0ff5d6e30d74c706e245d16c07ebbd772881
Charles Boutaud <cboutaud@users.noreply.github.com>
2024-03-05 14:40:26 +0100

    Update script

--- check-employement-tribunal.py
+++ check-employement-tribunal.py
@@ -29,71 +29,71 @@ cases_to_remove = {doc["Decision id"] for doc in documents_data if doc["Outcome"
 # Remove the relevant decisions
 decisions_data = [case for case in decisions_data if case["Decision id"] not in cases_to_remove]
 
-# # Open the care sponsors data
-# print("[*] Opening the sponsors data")
-# with open("data-out/all-skilled-home-care-sponsors.csv") as infile:
-# 	reader = csv.DictReader(infile)
-# 	header = reader.fieldnames
-# 	sponsors_data = [row for row in reader]
-# # Get the number of sponsors
-# sponsors_count = len(sponsors_data)
-# org_with_cases_found = 0
+# Open the care sponsors data
+print("[*] Opening the sponsors data")
+with open("data-out/all-skilled-home-care-sponsors.csv") as infile:
+	reader = csv.DictReader(infile)
+	header = reader.fieldnames
+	sponsors_data = [row for row in reader]
+# Get the number of sponsors
+sponsors_count = len(sponsors_data)
+org_with_cases_found = 0
 
-# # Blank list to store cases data
-# all_sponsors_cases = list()
-# # Loop through care sponsors
-# print("[*] Checking sponsors against employement tribunal data")
-# for i, sponsor in enumerate(sponsors_data):
-# 	org_name = sponsor["Organisation Name"]
-# 	# Get the id of employement tribunal decisions involving this org
-# 	org_tribunal_cases = [x for x in decisions_data if x["Defendant"] == org_name]
-# 	org_tribunal_cases_ids = [x["Decision id"] for x in org_tribunal_cases]
-# 	cases_str = " / ".join(org_tribunal_cases_ids)
-# 	# Get the count and id of the tribunal decisions
-# 	decisions_count = len(org_tribunal_cases)
-# 	if decisions_count > 0:
-# 		org_with_cases_found += 1
-# 	# Add the new data to the sponsor data
-# 	sponsor["Employement Tribunal decisions count"] = decisions_count
-# 	sponsor["Employement Tribunal decisions ids"] = cases_str
+# Blank list to store cases data
+all_sponsors_cases = list()
+# Loop through care sponsors
+print("[*] Checking sponsors against employement tribunal data")
+for i, sponsor in enumerate(sponsors_data):
+	org_name = sponsor["Organisation Name"]
+	# Get the id of employement tribunal decisions involving this org
+	org_tribunal_cases = [x for x in decisions_data if x["Defendant"] == org_name]
+	org_tribunal_cases_ids = [x["Decision id"] for x in org_tribunal_cases]
+	cases_str = " / ".join(org_tribunal_cases_ids)
+	# Get the count and id of the tribunal decisions
+	decisions_count = len(org_tribunal_cases)
+	if decisions_count > 0:
+		org_with_cases_found += 1
+	# Add the new data to the sponsor data
+	sponsor["Employement Tribunal decisions count"] = decisions_count
+	sponsor["Employement Tribunal decisions ids"] = cases_str
 
-# 	# Check if the decision was after the org appeared as a sponsor
-# 	first_appear = datetime.strptime(sponsor["First appeared"], "%Y-%m-%d")
-# 	for case in org_tribunal_cases:
-# 		decision_date = datetime.strptime(case["Decision date"], "%Y-%m-%d")
-# 		if first_appear < decision_date:
-# 			case["Decision after sponsor first appeared date"] = True
-# 		else:
-# 			case["Decision after sponsor first appeared date"] = False
+	# Check if the decision was after the org appeared as a sponsor
+	first_appear = datetime.strptime(sponsor["First appeared"], "%Y-%m-%d")
+	for case in org_tribunal_cases:
+		decision_date = datetime.strptime(case["Decision date"], "%Y-%m-%d")
+		if first_appear < decision_date:
+			case["Decision after sponsor first appeared date"] = True
+		else:
+			case["Decision after sponsor first appeared date"] = False
 	
-# 	# Add the org cases to the output list
-# 	all_sponsors_cases.extend(org_tribunal_cases)
+	# Add the org cases to the output list
+	all_sponsors_cases.extend(org_tribunal_cases)
 
-# 	# Print process update
-# 	index = i + 1
-# 	if index % 500 == 0:
-# 		update_msg = "[*] Checked {}/{} sponsors against employement tribunal data"
-# 		print(update_msg.format(index, sponsors_count))
-# 	# Conditional break used when testing
-# 	# if i > 99:
-# 		# break
+	# Print process update
+	index = i + 1
+	if index % 500 == 0:
+		update_msg = "[*] Checked {}/{} sponsors against employement tribunal data"
+		print(update_msg.format(index, sponsors_count))
+	# Conditional break used when testing
+	# if i > 99:
+		# break
 
-# print("[*] We've found employement tribunal decisions for {} orgs".format(org_with_cases_found))
-# # Re-write the sponsors data with the new datafields added
-# new_datafields = ["Employement Tribunal decisions count", "Employement Tribunal decisions ids"]
-# if new_datafields[0] not in header:
-# 	header.extend(new_datafields)
-# with open("data-out/all-skilled-home-care-sponsors.csv", "w") as outfile:
-# 	writer = csv.DictWriter(outfile, fieldnames=header)
-# 	writer.writeheader()
-# 	writer.writerows(sponsors_data)
+print("[*] We've found employement tribunal decisions for {} orgs".format(org_with_cases_found))
+# Re-write the sponsors data with the new datafields added
+new_datafields = ["Employement Tribunal decisions count", "Employement Tribunal decisions ids"]
+if new_datafields[0] not in header:
+	header.extend(new_datafields)
+with open("data-out/all-skilled-home-care-sponsors.csv", "w") as outfile:
+	writer = csv.DictWriter(outfile, fieldnames=header)
+	writer.writeheader()
+	writer.writerows(sponsors_data)
 
-# # Write the cases
-# decisions_header.append("Decision after sponsor first appeared date")
-# with open("data-out/sponsors-cases.csv", "w") as outfile:
-# 	writer = csv.DictWriter(outfile, fieldnames=decisions_header)
-# 	writer.writeheader()
-# 	writer.writerows(all_sponsors_cases)
+# Write the cases
+decisions_header.append("Decision after sponsor first appeared date")
+with open("data-out/sponsors-cases.csv", "w") as outfile:
+	writer = csv.DictWriter(outfile, fieldnames=decisions_header)
+	writer.writeheader()
+	writer.writerows(all_sponsors_cases)
 
-# print("[*] Task completed!")
+print("[*] Task completed!")
 
